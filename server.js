d9a4704529fde87ccfaa838cc69ca6f14d3f9910
heroyooi <heroyooi1018@gmail.com>
2021-09-07 13:23:48 +0900

    Architecture part Two

--- server.js
+++ server.js
@@ -1,41 +1,8 @@
 import { ApolloServer, gql } from "apollo-server";
-
-const typeDefs = gql`
-  type Movie {
-    id: Int
-    title: String
-    year: Int
-  }
-  type Query {
-    movies: [Movie]
-    movie: Movie
-  }
-  type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => [],
-    movie: () => ({ title: "Hello", year: 2021 }),
-  },
-  Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-    deleteMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-  },
-};
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
 server
